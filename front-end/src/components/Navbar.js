383ed4a694169b19742fd2061452b43fb4c45775
Abhishek-hash <54746811+Abhishek-hash@users.noreply.github.com>
2024-02-23 13:04:10 +0530

    Update Navbar.js

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -19,14 +19,14 @@ export default function Navbar() {
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-success">
         <div className="container-fluid">
-          <Link className="navbar-brand fs-1 fst-italic" to="/">Go-Feast</Link>
+          <Link className="navbar-brand fs-1 fst-italic" to="/home">Go-Feast</Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto mb-2">
               <li className="nav-item">
-                <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link active fs-5" aria-current="page" to="/home">Home</Link>
               </li>
               {
                 (localStorage.getItem("authToken")) ?
